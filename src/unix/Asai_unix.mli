f714d4877b57acc5c0f5cc57f0a2d94abdbe5993
Reed Mullanix <reedmullanix@gmail.com>
2022-06-10 09:11:02 -0700

    chore: document Asai_unix a bit

--- src/unix/Asai_unix.mli
+++ src/unix/Asai_unix.mli
@@ -1,6 +1,10 @@
-module StringTbl := Hashtbl.Make(String)
-
+(** Diagnostic display for UNIX terminals. *)
 open Asai
+
+(** {1 Display}
+    This module provides a single function for display Asai diagnostics to UNIX terminals. *)
+
+module StringTbl := Hashtbl.Make(String)
 
 module Make (ErrorCode : ErrorCode.S) : sig
   module Diagnostic := Diagnostic.Make(ErrorCode)
