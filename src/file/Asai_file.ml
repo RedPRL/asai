2e97b7d51dec13f4a4fe23e26bf748e431f88eec
favonia <favonia@gmail.com>
2022-08-09 16:46:46 -0500

    refactor: remove the functor interface

--- src/file/Asai_file.ml
+++ src/file/Asai_file.ml
@@ -2,39 +2,29 @@ open Bwd
 
 module Marked = Marked
 
-module type S =
-sig
-  module Code : Asai.Code.S
-  val format : splitting_threshold:int -> Code.t Asai.Diagnostic.t -> Code.t Marked.t
-end
+let format_sections ~splitting_threshold ~additional_marks span =
+  let marked_sections =
+    match span with
+    | None -> Flattener.empty
+    | Some sp -> Flattener.singleton (`Highlighted, sp)
+  in
+  let marked_sections =
+    (* add additional_marks *)
+    List.fold_right (fun sp -> Flattener.add (`Marked, sp)) additional_marks marked_sections
+  in
+  List.map Marker.mark_section @@ Flattener.flatten ~splitting_threshold marked_sections
 
-module Make (Code : Asai.Code.S) : S with module Code := Code
-=
-struct
-  let format_sections ~splitting_threshold ~additional_marks span =
-    let marked_sections =
-      match span with
-      | None -> Flattener.empty
-      | Some sp -> Flattener.singleton (`Highlighted, sp)
-    in
-    let marked_sections =
-      (* add additional_marks *)
-      List.fold_right (fun sp -> Flattener.add (`Marked, sp)) additional_marks marked_sections
-    in
-    List.map Marker.mark_section @@ Flattener.flatten ~splitting_threshold marked_sections
+let format_message ~splitting_threshold ~additional_marks (msg : _ Asai.Span.located) =
+  format_sections ~splitting_threshold ~additional_marks msg.loc, msg.value
 
-  let format_message ~splitting_threshold ~additional_marks (msg : _ Asai.Span.located) =
-    format_sections ~splitting_threshold ~additional_marks msg.loc, msg.value
-
-  let format ~splitting_threshold (d : Code.t Asai.Diagnostic.t) =
-    Reader.run @@ fun () ->
-    Marked.{
-      code = d.code;
-      severity = d.severity;
-      message = format_message ~splitting_threshold ~additional_marks:d.additional_marks d.message;
-      traces = Bwd.map (format_message ~splitting_threshold ~additional_marks:[]) d.traces;
-    }
-end
+let format ~splitting_threshold (d : 'code Asai.Diagnostic.t) =
+  Reader.run @@ fun () ->
+  Marked.{
+    code = d.code;
+    severity = d.severity;
+    message = format_message ~splitting_threshold ~additional_marks:d.additional_marks d.message;
+    traces = Bwd.map (format_message ~splitting_threshold ~additional_marks:[]) d.traces;
+  }
 
 module Internal =
 struct
--- src/file/Asai_file.mli
+++ src/file/Asai_file.mli
@@ -9,15 +9,8 @@
 
 module Marked = Marked
 
-module type S =
-sig
-  module Code : Asai.Code.S
-
-  (** [format ~splitting_threshold d] reads the file content and flatten the spans into marked text. *)
-  val format : splitting_threshold:int -> Code.t Asai.Diagnostic.t -> Code.t Marked.t
-end
-
-module Make (Code : Asai.Code.S) : S with module Code := Code
+(** [format ~splitting_threshold d] reads the file content and flatten the spans into marked text. *)
+val format : splitting_threshold:int -> 'code Asai.Diagnostic.t -> 'code Marked.t
 
 (**/**)
 
