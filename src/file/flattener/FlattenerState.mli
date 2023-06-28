8b7fc88ba3c172cf1f14d9edee6b9e724cb431f8
favonia <favonia@gmail.com>
2023-06-29 03:55:54 +0800

    style: even more refactoring

--- src/file/flattener/FlattenerState.mli
+++
@@ -1,8 +0,0 @@
-type t
-(** State of the flatterner *)
-
-val zero : t
-(** Initial states *)
-
-val style : t -> MarkedDiagnostic.style option
-val apply : MarkedDiagnostic.style * [`Begin | `End] -> t -> t
