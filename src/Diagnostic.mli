cec226f5ca9412cd273d93fbb0c6381e3d96ed89
favonia <favonia@gmail.com>
2023-09-23 21:38:47 -0500

    refactor: prioritize string-based API

--- src/Diagnostic.mli
+++ src/Diagnostic.mli
@@ -3,10 +3,10 @@
 (* @include *)
 include module type of DiagnosticData
 
-(** {1 Constructions} *)
+(** {1 Constructions of Messages} *)
 
-(** [text_of_string str] converts the string [str] into a text, turning each ['\n'] into a call to {!val:Format.pp_force_newline}. *)
-val text_of_string : string -> text
+(** [text str] converts the string [str] into a text, converting each ['\n'] into a call to {!val:Format.pp_force_newline}. *)
+val text : string -> text
 
 (** [textf format ...] constructs a text. It is an alias of {!val:Format.dprintf}. Note that there should not be any literal control characters (e.g., literal newline characters). *)
 val textf : ('a, Format.formatter, unit, text) format4 -> 'a
@@ -14,10 +14,10 @@ val textf : ('a, Format.formatter, unit, text) format4 -> 'a
 (** [ktextf kont format ...] is [kont (textf code format ...)]. It is an alias of {!val:Format.kdprintf}. *)
 val ktextf : (text -> 'b) -> ('a, Format.formatter, unit, 'b) format4 -> 'a
 
-(** [message_of_string str] converts the string [str] into a message.
+(** [message str] converts the string [str] into a message.
 
     @param loc The location of the message (usually the code) to highlight. *)
-val message_of_string : ?loc:Span.t -> string -> message
+val message : ?loc:Span.t -> string -> message
 
 (** [messagef format ...] constructs a message. Note that there should not be any literal control characters (e.g., literal newline characters).
 
@@ -29,14 +29,62 @@ val messagef : ?loc:Span.t -> ('a, Format.formatter, unit, message) format4 -> '
 
     @param loc The location of the message (usually the code) to highlight.
 *)
-val kmessagef : (message -> 'b) -> ?loc:Span.t -> ('a, Format.formatter, unit, 'b) format4 -> 'a
+val kmessagef : ?loc:Span.t -> (message -> 'b) -> ('a, Format.formatter, unit, 'b) format4 -> 'a
 
-(** {1 Utility Functions} *)
+(** {1 Constructions of Diagnostics} *)
 
-(** A convenience function that turns a {!type:severity} into a string. *)
+(** [of_message severity code message] constructs a diagnostic with the [message].
+
+    Example:
+    {[
+      make Warning `ChiError @@ message "Your Ch'i is critically low"
+    ]}
+
+    @param backtrace The backtrace (to overwrite the accumulative frames up to this point).
+    @param additional_messages Additional messages that part of the backtrace. For example, they can be bindings shadowed by the current one.
+*)
+val of_message : ?backtrace:backtrace -> ?additional_messages:message list -> severity -> 'code -> message -> 'code t
+
+(** [make severity code str] constructs a diagnostic with the message [str].
+
+    Example:
+    {[
+      make Warning `ChiError "Your Ch'i is critically low"
+    ]}
+
+    @param backtrace The backtrace (to overwrite the accumulative frames up to this point).
+    @param additional_messages Additional messages that part of the backtrace. For example, they can be bindings shadowed by the current one.
+*)
+val make : ?loc:Span.t -> ?backtrace:backtrace -> ?additional_messages:message list -> severity -> 'code -> string -> 'code t
+
+(** [makef severity code format ...] is [make severity code (messagef format ...)]. It formats the message and constructs a diagnostic out of it.
+
+    Example:
+    {[
+      makef Warning `ChiError "Your %s is critically low" "Ch'i"
+    ]}
+
+    @param loc The location of the text (usually the code) to highlight.
+    @param backtrace The backtrace (to overwrite the accumulative frames up to this point).
+    @param additional_messages Additional messages that part of the backtrace. For example, they can be bindings shadowed by the current one.
+*)
+val makef : ?loc:Span.t -> ?backtrace:backtrace -> ?additional_messages:message list -> severity -> 'code -> ('a, Format.formatter, unit, 'code t) format4 -> 'a
+
+(** [kmakef kont severity code format ...] is [kont (makef severity code format ...)].
+
+    @param loc The location of the text (usually the code) to highlight.
+    @param backtrace The backtrace (to overwrite the accumulative frames up to this point).
+    @param additional_messages Additional messages that part of the backtrace. For example, they can be bindings shadowed by the current one.
+*)
+val kmakef : ?loc:Span.t -> ?backtrace:backtrace -> ?additional_messages:message list -> ('code t -> 'b) -> severity -> 'code -> ('a, Format.formatter, unit, 'b) format4 -> 'a
+
+
+(** {1 Other Helper Functions} *)
+
+(** A convenience function that converts a {!type:severity} into its constructor name. For example, {!constructor:Warning} will be converted into the string ["Warning"]. *)
 val string_of_severity : severity -> string
 
-(** A convenience function that turns a {!type:text} into a string by formatting it with the maximum admissible margin. Note that the resulting string may contain newline characters. *)
+(** A convenience function that converts a {!type:text} into a string by formatting it with the maximum admissible margin. Note that the resulting string may contain newline characters and thus not suitable to . *)
 val string_of_text : text -> string
 
 (** A convenience function that maps the message code. This is helpful when using {!val:Logger.S.adopt}. *)
