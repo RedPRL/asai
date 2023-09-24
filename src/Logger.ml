6ba631e28259a8ec3f3d019bcc528c754c404fcc
favonia <favonia@gmail.com>
2023-09-24 00:26:56 -0500

    feat(Logger): remember the last location

--- src/Logger.ml
+++ src/Logger.ml
@@ -8,27 +8,42 @@ struct
 
   (* Backtraces *)
 
-  module Traces = Algaeff.Reader.Make (struct type nonrec env = Diagnostic.backtrace end)
+  module Traces = Algaeff.Reader.Make (struct type nonrec env = Span.t option * Diagnostic.backtrace end)
 
-  let get_backtrace = Traces.read
+  let get_loc() = fst @@ Traces.read()
 
-  let with_backtrace bt = Traces.run ~env:bt
+  let with_loc loc = Traces.scope @@ fun (_, bt) -> loc, bt
 
-  let trace_message msg = Traces.scope @@ fun bt -> bt <: msg
+  let merge_loc loc f =
+    match loc with
+    | None -> f()
+    | loc -> with_loc loc f
 
-  let trace ?loc str = trace_message @@ Diagnostic.message ?loc str
+  let get_backtrace() = snd @@ Traces.read()
+
+  let with_backtrace bt = Traces.scope @@ fun (loc, _) -> loc, bt
+
+  let trace_text ?loc text = Traces.scope @@ fun (l, bt) ->
+    let loc = match loc with None -> l | Some _ -> loc in
+    loc, bt <: {loc; value = text}
+
+  let trace_message (msg : Diagnostic.message) =
+    trace_text ?loc:msg.loc msg.value
+
+  let trace ?loc str = trace_text ?loc @@ Diagnostic.text str
 
   let tracef ?loc = Diagnostic.kmessagef trace_message ?loc
 
   (* Building messages *)
 
   let get_severity code = function None -> Code.default_severity code | Some severity -> severity
+  let get_merged_loc = function None -> get_loc() | loc -> loc
 
   let diagnostic ?severity ?loc ?(backtrace=get_backtrace()) ?additional_messages code str =
-    Diagnostic.make ?loc ~backtrace ?additional_messages (get_severity code severity) code str
+    Diagnostic.make ?loc:(get_merged_loc loc) ~backtrace ?additional_messages (get_severity code severity) code str
 
   let kdiagnosticf ?severity ?loc ?(backtrace=get_backtrace()) ?additional_messages k code =
-    Diagnostic.kmakef ?loc ~backtrace ?additional_messages k (get_severity code severity) code
+    Diagnostic.kmakef ?loc:(get_merged_loc loc) ~backtrace ?additional_messages k (get_severity code severity) code
 
   let diagnosticf ?severity ?loc ?backtrace ?additional_messages code =
     kdiagnosticf Fun.id ?severity ?loc ?backtrace ?additional_messages code
@@ -52,8 +67,8 @@ struct
 
   (* Runners *)
 
-  let run ?(init_backtrace=Emp) ~emit ~fatal f =
-    Traces.run ~env:init_backtrace @@ fun () ->
+  let run ?init_loc ?(init_backtrace=Emp) ~emit ~fatal f =
+    Traces.run ~env:(init_loc, init_backtrace) @@ fun () ->
     Effect.Deep.match_with f () @@ handler ~emit ~fatal
 
   let try_with ?(emit=emit_diagnostic) ?(fatal=fatal_diagnostic) f =
@@ -73,8 +88,9 @@ struct
   let fatalf ?severity ?loc ?backtrace ?additional_messages code =
     kdiagnosticf fatal_diagnostic ?severity ?loc ?backtrace ?additional_messages code
 
-  let adopt m (run : ?init_backtrace:_ -> emit:_ -> fatal:_ -> _) f =
+  let adopt m (run : ?init_loc:_ -> ?init_backtrace:_ -> emit:_ -> fatal:_ -> _) f =
     run f
+      ?init_loc:(get_loc())
       ~init_backtrace:(get_backtrace())
       ~emit:(fun d -> emit_diagnostic (m d))
       ~fatal:(fun d -> fatal_diagnostic (m d))
