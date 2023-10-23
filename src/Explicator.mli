b6d6246e89bea33af8bd4d2f2d792794ec4a3844
favonia <favonia@gmail.com>
2023-10-22 22:10:47 -0500

    docs(Explicator): fix docstring

--- src/Explicator.mli
+++ src/Explicator.mli
@@ -1,6 +1,6 @@
 include module type of ExplicatorSigs
 
-(* The default tag blending algorithm that chooses the more important tag based on priority. *)
+(** The default tag blending algorithm that chooses the more important tag based on priority. *)
 val default_blend : priority:('tag -> int) -> 'tag -> 'tag -> 'tag
 
 (** Making an explicator. *)
