472f096a5cc1cbce53ab930e30f5867935b163d5
favonia <favonia@gmail.com>
2023-10-25 19:30:56 -0500

    feat(Tty): detect NO_COLOR

--- src/tty/TtyStyle.ml
+++ src/tty/TtyStyle.ml
@@ -1,13 +1,24 @@
 open Notty
 open Notty.Infix
 
+let no_color_lazy = lazy begin
+  match Sys.getenv_opt "NO_COLOR" with
+  | None | Some "" -> false
+  | _ -> true
+end
+
+let no_color () = Lazy.force no_color_lazy
+
 let code (severity : Diagnostic.severity) : attr =
-  match severity with
-  | Hint -> A.fg A.blue
-  | Info -> A.fg A.green
-  | Warning -> A.fg A.yellow
-  | Error -> A.fg A.red
-  | Bug -> A.bg A.red ++ A.fg A.black
+  if no_color () then
+    A.st A.bold
+  else
+    match severity with
+    | Hint -> A.fg A.blue
+    | Info -> A.fg A.green
+    | Warning -> A.fg A.yellow
+    | Error -> A.fg A.red
+    | Bug -> A.bg A.red ++ A.fg A.black
 
 let message (severity : Diagnostic.severity) (tag : TtyTag.t) : attr =
   match tag with
@@ -19,6 +30,6 @@ let highlight (severity : Diagnostic.severity) : TtyTag.t option -> attr =
   | None -> A.empty
   | Some tag -> A.st A.underline ++ message severity tag
 
-let fringe = A.fg @@ A.gray 8
+let fringe = if no_color () then A.empty else A.fg @@ A.gray 8
 
-let indentation = A.fg @@ A.gray 8
+let indentation = if no_color () then A.empty else A.fg @@ A.gray 8
