9b0ecfdea989a647b6ec0499c901c54ea611994f
favonia <favonia@gmail.com>
2023-11-02 15:52:19 -0500

    docs(MinimumSigs): revise

--- src/MinimumSigs.ml
+++ src/MinimumSigs.ml
@@ -1,4 +1,6 @@
-(** This signature presents the minimum interface to use diagnostic handlers. Any module implementing {!module-type:Reporter.Message} or {!module-type:StructuredReporter.Message} will also implement this minimum interface. *)
+(** The minimum interface to use diagnostic handlers.
+
+    Any module implementing {{!module-type:Asai.Reporter.Message}[Reporter.Message]} or {!module-type:StructuredReporter.Message} will also implement this minimum interface. *)
 module type Message =
 sig
 
@@ -9,13 +11,27 @@ sig
   val short_code : t -> string
 end
 
-(** This is a template signature provided by asai. It is the minimum interface for different libraries and applications that use asai to work together. The author of the library you are using employs this template to hide almost everything from the public interface except the minimum API for you to adopt it. In the following documentation, "the library" refers to the library using this template, not asai. *)
+(** The minimum interface for libraries and applications to work together.
+
+    {b If you are the author of a library that uses asai,} it is recommended to wrap your reporter with this signature in the wrapper. For example, if you are using Dune, and your wrapper file is [CoolLibrary.ml(i)], use this to wrap your [Reporter]:
+    {[
+      module Reporter : Asai.MinimumSigs.Reporter
+    ]}
+
+    {b If you arrive here because the library you are using employs this template signature} to reveal only the minimum API, you can write the following code to connect the library's reporter to yours:
+    {[
+      Reporter.adopt (Diagnostic.map message_mapper) CoolLibrary.Reporter.run @@ fun () -> ...
+    ]}
+    Please take a look at our {{!page:quickstart}Quick Start Tutorial} about how to use a library that uses asai.
+*)
 module type Reporter =
 sig
 
   module Message : Message
+  (** The module for messages. *)
 
-  (** [run ~emit ~fatal f] runs the thunk [f], using [emit] to handle non-fatal diagnostics from the library before continuing the computation, and [fatal] to handle fatal diagnostics from the library that have aborted the computation. The recommended way to handle messages from the library is to use {!val:Asai.Reporter.S.adopt}:
+  val run : ?init_loc:Range.t -> ?init_backtrace:Diagnostic.backtrace -> emit:(Message.t Diagnostic.t -> unit) -> fatal:(Message.t Diagnostic.t -> 'a) -> (unit -> 'a) -> 'a
+  (** [run ~emit ~fatal f] runs the thunk [f], using [emit] to handle non-fatal diagnostics before continuing the computation, and [fatal] to handle fatal diagnostics that have aborted the computation. The recommended way to handle messages from a library that uses asai is to use {{!val:Asai.Reporter.S.adopt}[adopt]}:
       {[
         Reporter.adopt (Diagnostic.map message_mapper) The_library.Reporter.run @@ fun () -> ...
       ]}
@@ -24,5 +40,4 @@ sig
       @param init_backtrace The initial backtrace to start with. The default value is the empty backtrace.
       @param emit The handler of non-fatal diagnostics.
       @param fatal The handler of fatal diagnostics. *)
-  val run : ?init_loc:Range.t -> ?init_backtrace:Diagnostic.backtrace -> emit:(Message.t Diagnostic.t -> unit) -> fatal:(Message.t Diagnostic.t -> 'a) -> (unit -> 'a) -> 'a
 end
